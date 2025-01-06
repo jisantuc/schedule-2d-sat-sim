529296fbb80dc64a95c0e94cb3b397423464fc65
James Santucci <james.santucci@gmail.com>
2025-01-05 19:01:36 -0800

    test stdout producer

--- test/SatSim/Gen/ProducerSpec.hs
+++ test/SatSim/Gen/ProducerSpec.hs
@@ -2,14 +2,20 @@
 
 module SatSim.Gen.ProducerSpec where
 
+import Conduit (runConduit, (.|))
+import Control.Concurrent (threadDelay)
+import Control.Concurrent.Async (race)
+import Control.Monad (forever)
 import Data.Foldable (traverse_)
 import Data.Time (addUTCTime, getCurrentTime)
-import SatSim.Gen.Producer (genSchedulable)
+import SatSim.Gen.Producer (genSchedulable, stdoutBatchProducer, timeProducer)
+import SatSim.Quantities (Seconds (..))
 import SatSim.Schedulable (Schedulable (..))
+import System.IO.Silently (capture_)
 import Test.Hspec (Spec, describe, it, shouldBe)
 
 spec :: Spec
-spec = describe "GenSpec" $ do
+spec = describe "ProducerSpec" $ do
   it "generates the right number of targets" $
     getCurrentTime >>= \currentTime -> do
       targets <- genSchedulable currentTime 100 3
@@ -29,3 +35,13 @@ spec = describe "GenSpec" $ do
                 startCollectAfter > currentTime `shouldBe` True
             )
             targets
+  it "produces a stream of outputs" $
+    do
+      output <-
+        capture_
+          ( race
+              (forever . runConduit $ timeProducer 2 .| stdoutBatchProducer (Seconds 10))
+              (threadDelay 5000000)
+          )
+      -- in 5 seconds, producing every 2 seconds should have happened twice
+      (length . lines $ output) `shouldBe` 2
