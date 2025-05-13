d6688ca6cedd4710e60e309824326f19b858cdd4
James Santucci <james.santucci@gmail.com>
2025-05-13 12:02:11 -0700

    WIP -- really didn't want FlexibleContexts but maybe that's needed

--- src/SatSim/Consumer/AMQP.hs
+++ src/SatSim/Consumer/AMQP.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module SatSim.Consumer.AMQP where
 
 import Control.Concurrent (threadDelay)
 import Control.Monad.Catch (MonadCatch)
 import Control.Monad.IO.Class (MonadIO (liftIO))
+import Control.Monad.Trans.Control (MonadBaseControl)
 import Data.Aeson (eitherDecode)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
 import Data.These (These (..))
 import Network.AMQP
   ( Ack (..),
-    Envelope,
     ExchangeOpts (..),
     Message (..),
     QueueOpts (..),
@@ -32,8 +33,10 @@ import SatSim.Quantities (Seconds (..))
 import SatSim.Satellite (Satellite (..))
 import SatSim.Schedulable (Schedulable (..), ScheduleId (..))
 import SatSim.ScheduleRepository (ScheduleRepository (..))
+import qualified Streamly.Data.Fold as Fold
 import Streamly.Data.Stream (finallyIO)
 import qualified Streamly.Data.Stream as Stream
+import Streamly.Data.Stream.Prelude (maxThreads, parMergeBy)
 
 data Heartbeat m = Heartbeat {unheartbeat :: m (), interval :: Seconds}
 
@@ -42,10 +45,10 @@ beatForever (Heartbeat {unheartbeat, interval}) =
   Stream.repeatM (unheartbeat *> liftIO (threadDelay (floor interval * 1000000)))
 
 consumeBatchesFromExchange ::
-  (ScheduleRepository m, MonadIO m, MonadCatch m) =>
+  (ScheduleRepository m, MonadIO m, MonadCatch m, MonadBaseControl IO m) =>
   Satellite ->
   Text ->
-  Heartbeat IO ->
+  Heartbeat m ->
   m ()
 consumeBatchesFromExchange satellite exchangeName' hb = do
   (conn, chan, queue) <- liftIO $ do
@@ -57,21 +60,17 @@ consumeBatchesFromExchange satellite exchangeName' hb = do
     pure (conn, chan, queue)
 
   let beat = beatForever hb
-  let consumer = finallyIO (closeConnection conn) (Stream.mapM callback (consume chan queue Ack))
-  liftIO $ print "oh no"
+  let bracketed = finallyIO (closeConnection conn) (Stream.mapM callback $ consume chan queue Ack)
+  Stream.fold Fold.drain $ parMergeBy (maxThreads 4) compare beat bracketed
   where
-    callback :: (ScheduleRepository m, MonadIO m) => (Message, Envelope) -> m ()
     callback (msg, envelope) =
       let scheduleNewCandidates batch = do
             schedule <- fromMaybe mempty <$> readSchedule (ScheduleId "schedule-key")
             let newSchedule = scheduleOn satellite batch schedule
             case newSchedule of
-              This _ -> liftIO $ do
-                putStrLn "nothing scheduled"
-              That sched -> do
-                writeSchedule (ScheduleId "schedule-key") sched
-              These _ sched -> do
-                writeSchedule (ScheduleId "schedule-key") sched
+              This _ -> liftIO $ putStrLn "nothing scheduled"
+              That sched -> writeSchedule (ScheduleId "schedule-key") sched
+              These _ sched -> writeSchedule (ScheduleId "schedule-key") sched
        in do
             either
               (liftIO . putStrLn)
