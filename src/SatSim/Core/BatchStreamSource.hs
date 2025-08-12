cd5d7ddb85d97848c81f160f9f4bd0c1aff2dfdc
James Santucci <james.santucci@gmail.com>
2025-08-11 22:38:20 -0700

    Add local batchstreamsource

--- src/SatSim/Core/BatchStreamSource.hs
+++ src/SatSim/Core/BatchStreamSource.hs
@@ -4,12 +4,15 @@
 
 module SatSim.Core.BatchStreamSource where
 
-import Control.Monad.Trans.Reader (ReaderT)
+import Control.Monad.Trans.Reader (ReaderT, ask, Reader)
 import SatSim.PubSub (RabbitMQConnectInfo, consumeBatches)
 import SatSim.Schedulable (Schedulable)
 import Streamly.Data.Stream (Stream)
+import qualified Streamly.Data.Stream as Stream
 import Control.Monad.Catch (MonadCatch)
 import Control.Monad.IO.Class (MonadIO)
+
+newtype StaticBatchStream = StaticBatchStream { batchesToEmit :: [[Schedulable]] } deriving (Eq, Show)
 
 class BatchStreamSource m where
   batches :: Stream m [Schedulable]
@@ -17,3 +20,6 @@ class BatchStreamSource m where
 instance (MonadIO m, MonadCatch m) => BatchStreamSource (ReaderT RabbitMQConnectInfo m)
   where
   batches = consumeBatches
+
+instance BatchStreamSource (Reader StaticBatchStream) where
+  batches = Stream.concatMap (Stream.fromList . batchesToEmit) (Stream.fromEffect ask)
